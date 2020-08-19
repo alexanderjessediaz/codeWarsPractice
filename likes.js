8de4cbe75949496f225d31d97ae432aee0930532
AJ <alexanderjessediaz@gmail.com>
2020-08-19 16:12:24 -0600

    add tests and reformat first problem in likes.js

--- likes.js
+++ likes.js
@@ -1,3 +1,6 @@
+// codewars
+// 8/19 
+
 // You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
 
 // Implement a function likes :: [String] -> String, which must take in input array, containing the names of people who like an item. It must return the display text as shown in the examples:
@@ -9,8 +12,6 @@
 // likes ["Alex", "Jacob", "Mark", "Max"] // must be "Alex, Jacob and 2 others like this"
 // For 4 or more names, the number in and 2 others simply increases.
 
-// Solution
-// 8/19
 
 function likes(names) {
     return names.length == 0 ? 'no one likes this'
@@ -19,3 +20,14 @@ function likes(names) {
       : names.length == 3 ? `${names[0]}, ${names[1]} and ${names[2]} like this`
       : `${names[0]}, ${names[1]} and ${names.length-2} others like this`
   }
+
+  // tests
+//   describe('example tests', function() {
+//     it('should return correct text', function() {
+//       Test.assertEquals(likes([]), 'no one likes this');
+//       Test.assertEquals(likes(['Peter']), 'Peter likes this');
+//       Test.assertEquals(likes(['Jacob', 'Alex']), 'Jacob and Alex like this');
+//       Test.assertEquals(likes(['Max', 'John', 'Mark']), 'Max, John and Mark like this');
+//       Test.assertEquals(likes(['Alex', 'Jacob', 'Mark', 'Max']), 'Alex, Jacob and 2 others like this');
+//     });
+//   });
