86f7357ecdc9edb0fcb956a6befbe5d19e7b7344
AJ <alexanderjessediaz@gmail.com>
2020-08-19 19:50:53 -0600

    jaden string prototype problem

--- codeWarsKatas.js
+++ codeWarsKatas.js
@@ -1,5 +1,5 @@
 // codewars
-// 8/19 
+// 8/19, Problem: like
 
 // You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
 
@@ -12,6 +12,8 @@
 // likes ["Alex", "Jacob", "Mark", "Max"] // must be "Alex, Jacob and 2 others like this"
 // For 4 or more names, the number in and 2 others simply increases.
 
+//MY SOLUTION
+
 
 function likes(names) {
     return names.length == 0 ? 'no one likes this'
@@ -21,7 +23,7 @@ function likes(names) {
       : `${names[0]}, ${names[1]} and ${names.length-2} others like this`
   }
 
-  // tests
+  // TESTS
 //   describe('example tests', function() {
 //     it('should return correct text', function() {
 //       Test.assertEquals(likes([]), 'no one likes this');
@@ -31,3 +33,43 @@ function likes(names) {
 //       Test.assertEquals(likes(['Alex', 'Jacob', 'Mark', 'Max']), 'Alex, Jacob and 2 others like this');
 //     });
 //   });
+
+
+// 8/19, Problem: jaden string prototype
+
+// Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
+
+// Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+
+// Example:
+
+// Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+// Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+
+
+//MY FIRST ATTEMPT
+
+// const str = "the little frog"
+
+// String.prototype.toJadenCase = function () {
+//  const wordArray = str.trim().split(" ")
+//  const capSentence = wordArray.map(word => {
+//   let capLetter = word[0].toUpperCase()
+//   return capLetter + word.slice(1)
+// })
+//  const jadenWords = capSentence.join(" ") 
+// };
+
+// console.log(str.toJadenCase())
+
+//MY SOLUTION
+
+
+
+//TESTS
+// var str = "How can mirrors be real if our eyes aren't real";
+// Test.assertEquals(str.toJadenCase(), "How Can Mirrors Be Real If Our Eyes Aren't Real");
+
+
+
+
