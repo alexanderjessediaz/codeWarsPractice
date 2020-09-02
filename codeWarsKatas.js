1e1f5da1ec0c0ce83065a18185963583f602009b
AJ <alexanderjessediaz@gmail.com>
2020-09-02 13:34:08 -0600

    add string replace problem

--- codeWarsKatas.js
+++ codeWarsKatas.js
@@ -324,3 +324,50 @@ function descendingOrder(n){
 //     }
 //   }, 0)
 // };
+
+
+// 9/2 PROBLEM format strings
+
+// Given: an array containing hashes of names
+
+// Return: a string formatted as a list of names separated by commas except for the last two names, which should be separated by an ampersand.
+
+// Example:
+
+// list([ {name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'} ])
+// // returns 'Bart, Lisa & Maggie'
+
+// list([ {name: 'Bart'}, {name: 'Lisa'} ])
+// // returns 'Bart & Lisa'
+
+// list([ {name: 'Bart'} ])
+// // returns 'Bart'
+
+// list([])
+// // returns ''
+
+// SOLUTION
+
+// function list(names){
+//   return names.reduce(function(prev, current, index, array){
+//     if (index === 0){
+//       return current.name;
+//     }
+//     else if (index === array.length - 1){
+//       return prev + ' & ' + current.name;
+//     } 
+//     else {
+//       return prev + ', ' + current.name;
+//     }
+//   }, '');
+// }
+
+// TESTS
+// Test.assertEquals(list([{name: 'Bart'},{name: 'Lisa'},{name: 'Maggie'},{name: 'Homer'},{name: 'Marge'}]), 'Bart, Lisa, Maggie, Homer & Marge',
+// "Must work with many names")
+// Test.assertEquals(list([{name: 'Bart'},{name: 'Lisa'},{name: 'Maggie'}]), 'Bart, Lisa & Maggie',
+// "Must work with many names")
+// Test.assertEquals(list([{name: 'Bart'},{name: 'Lisa'}]), 'Bart & Lisa', 
+// "Must work with two names")
+// Test.assertEquals(list([{name: 'Bart'}]), 'Bart', "Wrong output for a single name")
+// Test.assertEquals(list([]), '', "Must work with no names")
