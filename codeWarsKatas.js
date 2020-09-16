01d52ba854a72a7d6dee87c2827ea9ef84a87afb
AJ <alexanderjessediaz@gmail.com>
2020-09-16 14:00:53 -0600

    add recycle problem

--- codeWarsKatas.js
+++ codeWarsKatas.js
@@ -656,3 +656,143 @@ function filterDuplicates(word){
 //   Test.assertDeepEquals(dup(["putteellinen","keenness"]), ['putelinen','kenes']);
 //   Test.assertDeepEquals(dup(["kelless","voorraaddoosspullen","achcha"]), ['keles','voradospulen','achcha']);
 //   });
+// 9/16
+// PROBLEM Let's Recycle
+
+// You will be given an array of object, every object might be composed like that:
+
+// {
+//   type: 'box',
+//   material: 'paper'
+// }
+// or like that:
+
+// {
+//   type: 'bottle',
+//   material: 'glass',
+//   secondMaterial: 'paper'
+// }
+// Those are the materials that you might encounter: paper, glass, organic, plastic.
+
+// An object can be made by one or two materials.
+
+// If an object is made of two materials recycle it on BOTH of the correct bins.
+
+// Recycle all the objects in a new nested array where the first nested array will contain paper, the second glass, the third organic and the fourth plastic, so given this array of objects:
+
+// [
+// {
+//   type: 'rotten apples',
+//   material: 'organic'
+// },
+// {
+//   type: 'out of date yogurt',
+//   material: 'organic',
+//   secondMaterial: 'plastic'
+// },
+// {
+//   type: 'wine bottle',
+//   material: 'glass',
+//   secondMaterial: 'paper'
+// },
+// {
+//   type: 'amazon box',
+//   material: 'paper'
+// },
+// {
+//   type: 'beer bottle',
+//   material: 'glass',
+//   secondMaterial: 'paper'
+// }
+// ]
+// You should return this nested array:
+
+// [['wine bottle', 'amazon box', 'beer bottle'], ['wine bottle', 'beer bottle'], ['rotten apples', 'out of date yogurt'], ['out of date yogurt']]
+// Rules:
+
+// -There will always be the type and material property on every object, in some object there will be the secondMaterial property.
+
+// -All the input provided will always have one or more valid object.
+
+// -Always return a nested array with all the four bins inside even if they are empty, example: [['wine bottle', 'amazon box', 'beer bottle'], [], ['rotten apples', 'out of date yogurt'], []]
+
+// -Put the items in the bins in the same order that you find it.
+
+// P.S Don't freak out if you encounter an organic bottle or a plastic apple, we are making some biotech experiments...
+
+// SOLUTION
+
+function paperSort(array) {
+  return array.filter(trash => {
+   if (
+     trash.material === "paper" || trash.secondMaterial === "paper" 
+     ) return true
+ }).map(trash => {
+   return trash.type
+ })
+}
+
+function glassSort(array) {
+  return array.filter(trash => {
+   if (
+     trash.material === "glass" || trash.secondMaterial === "glass" 
+     ) return true
+ }).map(trash => {
+   return trash.type
+ })
+}
+
+function organicSort(array) {
+  return array.filter(trash => {
+   if (
+     trash.material === "organic" || trash.secondMaterial === "organic" 
+     ) return true
+ }).map(trash => {
+   return trash.type
+ })
+}
+
+function plasticSort(array) {
+  return array.filter(trash => {
+   if (
+     trash.material === "plastic" || trash.secondMaterial === "plastic" 
+     ) return true
+ }).map(trash => {
+   return trash.type
+ })
+}
+
+function recycle(array){
+  const recycledTrash = [paperSort(array),glassSort(array),organicSort(array),plasticSort(array)]
+  return recycledTrash
+}
+
+//TESTS
+Test.assertDeepEquals(recycle([
+  {
+    type: 'rotten apples',
+    material: 'organic'
+  },
+  {
+    type: 'out of date yogurt',
+    material: 'organic',
+    secondMaterial: 'plastic'
+  },
+  {
+    type: 'wine bottle',
+    material: 'glass',
+    secondMaterial: 'paper'
+  },
+  {
+    type: 'amazon box',
+    material: 'paper'
+  },
+  {
+    type: 'beer bottle',
+    material: 'glass',
+    secondMaterial: 'paper'
+  }
+  ]), [ [ 'wine bottle', 'amazon box', 'beer bottle' ],
+    [ 'wine bottle', 'beer bottle' ],
+    [ 'rotten apples', 'out of date yogurt' ],
+    [ 'out of date yogurt' ] ])
